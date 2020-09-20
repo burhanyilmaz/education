73b7ee44cb5245d09c1a81292297d17a9a96a197
Burhan Yılmaz <wburhany@gmail.com>
2020-09-20 17:50:08 +0300

    Appcenter postclonse script updated.

--- student/appcenter-post-clone.sh
+++ student/appcenter-post-clone.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Metro config script."
-pwd
-cd student
-sed -i "metro.config-backup.js" "s/watchFolders,//g" metro.config.js 
-cd ..
+# pwd
+# cd student
+sed -i -e 's/watchFolders,//g' ./student/metro.config.js 
+# sed -i "metro.config-backup.js" "s/watchFolders,//g" metro.config.js 
+# cd ..
