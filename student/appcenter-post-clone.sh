3834ec759fb94cf47fb1966cf7114d8d0dfe65fc
Burhan Yılmaz <wburhany@gmail.com>
2020-09-21 21:36:37 +0300

    updated sh

--- student/appcenter-post-clone.sh
+++ student/appcenter-post-clone.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 echo "Metro config script."
-pwd
-# sed -i -e 's/watchFolders,//g' metro.config.js
-cd ..
-yarn install 
-cd student
-yarn install 
+
+sed -i -e 's/watchFolders,//g' metro.config.js
